c82528bb411162ff38527f7a16aeb2dccdbb7101
Suhani Chawla <suhanichawla2000@gmail.com>
2020-05-22 02:07:33 +0530

    week3 q7

---
+++ Week3/CountSquareSubmatricesWithAllOnes.java
@@ -0,0 +1,18 @@
+package mayLeetCode;
+
+public class CountSquareSubmatricesWithAllOnes {
+	public int countSquares(int[][] matrix) {
+        int dp[][] = new int[matrix.length + 1][matrix[0].length + 1];
+       int sum = 0;
+       
+       for(int i = 1; i <= matrix.length; i++){
+           for(int j = 1; j <= matrix[0].length; j++){
+               if(matrix[i - 1][j - 1] != 0){
+                   sum += (dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1);
+               }
+           }
+       }
+       return sum;
+   }
+
+}
